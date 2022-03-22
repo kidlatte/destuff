3fdc58676fcce85c557f614180b06819a6a32281
kidlatte <kidlattez@gmail.com>
2022-03-23 02:50:53 +0800

    add sqlite db

---
+++ Server/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Destuff.Server.Data.Entities;
+
+public class ApplicationUser: IdentityUser
+{
+
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,43 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.EntityFrameworkCore;
+using Destuff.Server.Data;
+using Destuff.Server.Data.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+
+var path = configuration["DOTNET_RUNNING_IN_CONTAINER"] == "true" ?
+    "/config" :
+    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+var folder = Path.Join(path, "Destuff");
+Console.WriteLine($"folder: {folder}");
+Directory.CreateDirectory(folder);
+
+var dbPath = Path.Join(folder, "destuff.db");
+var connString = $"Data Source={dbPath}";
+Console.WriteLine($"connString: {connString}");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connString));
+
+builder.Services
+    .AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedEmail = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Migrate datacontext changes
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dataContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
