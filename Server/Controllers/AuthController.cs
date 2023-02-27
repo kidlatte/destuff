6ddbd1ef5c6eb8bddacb899901e8fe10dcfb0097
kidlatte <kidlattez@gmail.com>
2023-02-28 00:23:04 +0800

    create users controller

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -31,43 +31,7 @@ public class AuthController : ControllerBase
         _logger = logger;
     }
 
-    [Authorize]
-    [HttpPost, Route(ApiRoutes.Users)]
-    public async Task<ActionResult<BlazorGridResult<UserModel>>> FetchUsers(BlazorGridRequest request)
-    {
-        var query = _userManager.Users.AsQueryable();
-
-        if (!string.IsNullOrEmpty(request.Query))
-            query = query.Where(x => x.UserName != null && x.UserName.StartsWith(request.Query));
-
-        var count = await query.CountAsync();
-        if (count == 0)
-        {
-            return Ok(new BlazorGridResult<UserModel> 
-            {
-                Data = new List<UserModel>(),
-                TotalCount = 0
-            });
-        }
-
-        // Apply ordering
-        if (request.OrderBy == null)
-            query = query.OrderBy(x => x.UserName);
-        else if (request.OrderByDescending)
-            query = query.OrderByDescending(request.OrderBy);
-        else
-            query = query.OrderBy(request.OrderBy);
-
-        // Apply paging
-        var rows = query.Skip(request.Offset).Take(request.Length)
-            .Select(x => new UserModel { UserName = x.UserName }).ToList();
-
-        return Ok(new BlazorGridResult<UserModel>
-        {
-            Data = rows,
-            TotalCount = count
-        });
-    }
+    
 
     [HttpPost, Route(ApiRoutes.AuthLogin)]
     public async Task<ActionResult<AuthTokenModel>> Login([FromBody] LoginModel model)
@@ -161,21 +125,5 @@ public class AuthController : ControllerBase
             // return error message if there was an exception
             return BadRequest(new { message = ex.Message });
         }
-    }
-
-    [Authorize]
-    [HttpDelete("{userName}")]
-    public async Task<IActionResult> DeleteUser(string userName)
-    {
-        var user = await _userManager.FindByNameAsync(userName);
-        if (user == null)
-            return BadRequest();
-
-        var result = await _userManager.DeleteAsync(user);
-        return Ok(new IdentityResultModel
-        {
-            Succeeded = result.Succeeded,
-            Errors = result.Errors.Select(x => x.Description).ToList()
-        });
     }
 }
---
+++ Server/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using BlazorGrid.Abstractions;
+using BlazorGrid.Abstractions.Extensions;
+using Destuff.Server.Data;
+using Destuff.Server.Services;
+using Destuff.Shared;
+using Destuff.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Destuff.Server.Controllers;
+
+[Route(ApiRoutes.Users)]
+[ApiController]
+[Authorize]
+public class UsersController : BaseController
+{
+    public UsersController(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<BlazorGridResult<UserModel>>> FetchUsers(BlazorGridRequest request)
+    {
+        var query = Context.Users.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.Query))
+            query = query.Where(x => x.UserName != null && x.UserName.StartsWith(request.Query));
+
+        var count = await query.CountAsync();
+        if (count == 0)
+        {
+            return Ok(new BlazorGridResult<UserModel>
+            {
+                Data = new List<UserModel>(),
+                TotalCount = 0
+            });
+        }
+
+        // Apply ordering
+        if (request.OrderBy == null)
+            query = query.OrderBy(x => x.UserName);
+        else if (request.OrderByDescending)
+            query = query.OrderByDescending(request.OrderBy);
+        else
+            query = query.OrderBy(request.OrderBy);
+
+        // Apply paging
+        var rows = query.Skip(request.Offset).Take(request.Length)
+            .Select(x => new UserModel { UserName = x.UserName }).ToList();
+
+        return Ok(new BlazorGridResult<UserModel>
+        {
+            Data = rows,
+            TotalCount = count
+        });
+    }
+
+
+    [HttpDelete("{userName}")]
+    public async Task<IActionResult> DeleteUser(string userName)
+    {
+        var user = await Context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+        if (user == null)
+            return BadRequest();
+
+        var result = Context.Users.Remove(user);
+        await Context.SaveChangesAsync();
+
+        return Ok(new IdentityResultModel
+        {
+            Succeeded = true
+        });
+    }
+
+}
