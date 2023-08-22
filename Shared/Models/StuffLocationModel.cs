0ef55b8f92919affd985e208f4b15bcc221a60b6
kidlatte <kidlattez@gmail.com>
2023-08-23 07:24:12 +0800

    reuse stuff location components

--- Shared/Models/StuffLocationModel.cs
+++ Shared/Models/StuffLocationModel.cs
@@ -12,15 +12,13 @@ public class StuffLocationRequest
     public int Count { get; set; }
 }
 
-public class StuffLocationModel
-{
-    public required LocationListItem Location { get; set; }
-    public required StuffBasicModel Stuff { get; set; }
-    public int Count { get; set; }
-}
-
 public class StuffLocationBasicModel
 {
     public required LocationListItem Location { get; set; }
     public int Count { get; set; }
+}
+
+public class StuffLocationModel : StuffLocationBasicModel
+{
+    public required StuffBasicModel Stuff { get; set; }
 }
