afc3da166c1b2fcd8aec105fe5285bb33ca24ded
kidlatte <kidlattez@gmail.com>
2023-11-21 17:57:46 +0800

    add part create

--- Client/Services/StuffTabs.cs
+++ Client/Services/StuffTabs.cs
@@ -3,6 +3,7 @@ namespace Destuff.Client.Services;
 internal static class StuffTabs
 {
     public const string? Details = null;
+    public const string Parts = "parts";
     public const string Locations = "locations";
     public const string Purchases = "purchases";
     public const string Events = "events";
